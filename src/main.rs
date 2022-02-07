2d9312e08ba54814176647f7415cb9107bb8f034
Ameer Taweel <ameertaweel2002@gmail.com>
2022-02-08 02:26:57 +0300

    feat: Define download subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use url::Url;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(
+	name = "Celeb-17",
+	about = "No Nonsense Command-Line Music Player Written in Rust."
+)]
+enum Options {
+	Download {
+		#[structopt(parse(try_from_str))]
+		url: Url
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	let options = Options::from_args();
+
+	match options {
+		Options::Download { url } => download(url)
+	}
+}
+
+fn download(url: Url){
+	println!("{}", url);
 }
