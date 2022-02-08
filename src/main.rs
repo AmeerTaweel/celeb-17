a424c9b22c64ab5a169e9f12d0d7c9108fb2c06f
Ameer Taweel <ameertaweel2002@gmail.com>
2022-02-08 03:54:55 +0300

    feat: Write prompt_user()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use fuzzy_matcher::FuzzyMatcher;
+use fuzzy_matcher::skim::SkimMatcherV2;
+use inquire::{Text, type_aliases::Suggester};
 use url::Url;
 use structopt::StructOpt;
 
@@ -22,5 +25,30 @@ fn main() {
 }
 
 fn download(url: Url){
+fn prompt_user(prompt: &str, suggestions: &[&str]) -> String {
+	let matcher = SkimMatcherV2::default();
+
+    let suggester: Suggester = &|input| {
+		let mut suggestions: Vec<(&&str, i64)> = suggestions.iter()
+			// Fuzzy match with user input
+			.map(|i| (i, matcher.fuzzy_match(i, input).unwrap_or_default()))
+			// Remove unmatching suggestions
+			.filter(|(i, score)| *score > 0)
+			.collect();
+		// Sort by score
+		suggestions.sort_by(|(_, a), (_, b)| b.cmp(a));
+		suggestions.iter().map(|(i, score)| i.to_string()).collect()
+	};
+
+	let answer = Text::new(prompt)
+		.with_suggester(suggester)
+		.prompt();
+
+	match answer {
+		Ok(answer) => answer,
+		Err(error) => std::process::exit(1)
+	}
+}
+
 	println!("{}", url);
 }
