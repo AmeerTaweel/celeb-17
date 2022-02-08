26bc1cc27733037255533088deff7448db6a9dc7
Ameer Taweel <ameertaweel2002@gmail.com>
2022-02-08 11:55:57 +0300

    feat: Update db module

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,25 @@
 // Database
 
+use crate::io;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
 use std::{ env, fs, path::PathBuf };
+
+#[derive(Serialize, Deserialize)]
+struct Song {
+	id: u64,
+	name: String,
+	artists: Vec<String>,
+	tags: Vec<String>,
+	version_of: u64
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all(deserialize = "snake_case", serialize = "camelCase"))]
+struct Database {
+	music_dir_location: String,
+	songs: Vec<Song>
+}
 
 pub fn get_path() -> PathBuf {
 	let db_path = env::var("XDG_CONFIG_HOME")
@@ -16,10 +35,12 @@ pub fn exists() -> bool {
 }
 
 pub fn init() {
+	let music_dir_location = io::prompt("Where is your music directory located?", &vec![]);
 	let db_path = get_path();
 	let db_dir = db_path.parent().unwrap();
 	fs::create_dir_all(db_dir).unwrap();
-	fs::write(db_path, "{}").unwrap();
+	let db = Database { music_dir_location, songs: vec![] };
+	fs::write(db_path, serde_json::to_string_pretty(&db).unwrap()).unwrap();
 }
 
 pub fn ensure() {
