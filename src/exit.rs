7ea29f6a4a5fdbd8874fc169feb0301e4b949d6c
Ameer Taweel <ameertaweel2002@gmail.com>
2022-02-09 10:20:54 +0300

    feat: Create exit modul

---
+++ src/exit.rs
@@ -0,0 +1,30 @@
+use crate::log;
+use std::process;
+
+pub fn with_error(msg: &str) -> ! {
+	log::error(msg);
+	process::exit(1);
+}
+
+pub fn with_warn(msg: &str) -> ! {
+	log::warn(msg);
+	process::exit(1);
+}
+
+pub fn with_success(msg: &str) -> ! {
+	log::success(msg);
+	process::exit(0);
+}
+
+pub trait UnwrapOrExit<T> {
+	fn unwrap_or_exit_with_error(self, msg: &str) -> T;
+}
+
+impl<T, E> UnwrapOrExit<T> for Result<T, E> {
+	fn unwrap_or_exit_with_error(self, msg: &str) -> T {
+		match self {
+			Ok(v) => v,
+			Err(_) => with_error(msg)
+		}
+	}
+}
